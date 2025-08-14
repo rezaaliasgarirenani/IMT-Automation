82cdb43ea09dea37798d3ed3bac6ecc6bc0980b3
rezaaliasgarirenani <siavash11sia@gmail.com>
2025-08-14 19:57:06 +0300

    cv zurich instrument 1.0

--- Automation_Apps_Codes/CV_Zurich/eaxmple_codes/zurich_test_id.m
+++ Automation_Apps_Codes/CV_Zurich/eaxmple_codes/zurich_test_id.m
@@ -2,10 +2,9 @@ function device= zurich_test_id(~)
 
 clear ziDAQ;
 device_id=('dev5478');
-supported_apilevel = 6;
+supported_apilevel = 5;
 % Create an API session; connect to the correct Data Server for the device.
 [device, ~] = ziCreateAPISession(device_id, supported_apilevel);
 ziApiServerVersionCheck();
-
 
 end
