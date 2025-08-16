ee62684134c8f29a6444d789e2ddca2e36f7631e
rezaaliasgarirenani <siavash11sia@gmail.com>
2025-08-16 05:08:28 +0300

    cv zurich 1.2

--- Automation_Apps_Codes/CV_Zurich/eaxmple_codes/examplecodes.m
+++ Automation_Apps_Codes/CV_Zurich/eaxmple_codes/examplecodes.m
@@ -1,10 +1,11 @@
 clc; clear;
 device = 'dev5478';
 ziCreateAPISession(device, 5)
-ziDAQ('listNodes', ['/' device ], 0)
+% ziDAQ('connect','127.0.0.1', 8006, 5)
+% ziDAQ('listNodes', ['/' device ], 0)
 
 % example_connect('dev5478')
 % example_connect_config('dev5478')
 % example_save_device_settings_simple('dev5478')
-% example_sweeper('dev5478')
+example_sweeper('dev5478')
 % example_poll_impedance('dev5478')
